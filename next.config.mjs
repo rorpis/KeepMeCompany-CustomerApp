0329e6ada4cb664130bed60166763e769218ac6c
Rodrigo Orpis <r.orpis@gmail.com>
2024-12-03 16:21:46 -0300

    env in next config for build process

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_FIREBASE_CONFIG: process.env.NEXT_PUBLIC_FIREBASE_CONFIG,
+    NEXT_PUBLIC_FIREBASE_CONFIG_apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_apiKey,
+    NEXT_PUBLIC_FIREBASE_CONFIG_appId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_appId,
+    NEXT_PUBLIC_FIREBASE_CONFIG_authDomain: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_authDomain,
+    NEXT_PUBLIC_FIREBASE_CONFIG_measurementId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_measurementId,
+    NEXT_PUBLIC_FIREBASE_CONFIG_messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_messagingSenderId,
+    NEXT_PUBLIC_FIREBASE_CONFIG_projectId: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_projectId,
+    NEXT_PUBLIC_FIREBASE_CONFIG_storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_storageBucket,
+  },
+};
 
 export default nextConfig;
